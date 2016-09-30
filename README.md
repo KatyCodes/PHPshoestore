aad69bce57113eb0f4e00382711c60fa138e7046
Katy Henning <katyannhening@gmail.com>
2016-09-30 08:31:13 -0700

    adds initial README with specs and initial vendor files

---
+++ README.md
@@ -0,0 +1,58 @@
+# Shoe Stores
+
+
+#### _A to do list using MySQL, September 26, 2016_
+
+#### By _**Katy Henning**_
+
+## Description
+
+A site that shows a chain of shoe stores and the brands that are sold at those stores.
+
+## Setup/Installation Requirements
+
+* Clone the repository
+* Using the command line, navigate to the project's root directory
+* Install dependencies by running $ composer install
+* Navigate to the /web directory and start a local server with $ php -S localhost:8000
+* Open a browser and go to the address http://localhost:8000 to view the application
+
+## Specifications
+
+* The program takes the input of a new shoe brand:
+  * Input: "Nike"
+  * Output: "Nike"
+
+* The program takes a new store and returns that store info:
+  * Input: "SW 5th and Washington"
+  * Output: "SW 5th and Washington"
+
+* The program can add shoe brands to the stores list:
+  * Input: "SW 5th and Washington" now sells Nike
+  * Output: show all brands at "SW 5th and Washington": "Nike"
+
+* The program can add stores to the shoes list:
+  * Input: "SW 5th and Washington" now sells Nike
+  * Output: You can buy Nike at the SW 5th and Washington location
+
+
+## Known Bugs
+
+There are no known bugs at this time.
+
+## Support and Contact Details
+
+For questions or comments, please contact me through GitHub.
+
+## Technologies Used
+
+* _PHP_
+* _Silex_
+* _Twig_
+* _Bootstrap_
+* _MySQL_
+
+### License
+
+*This website is licensed under the MIT license.*  
+Copyright (c) 2016 **_Katy Henning_**
---
+++ src/Brand.php
@@ -0,0 +1,96 @@
+<?php
+    class Brand
+    {
+        private $id;
+        private $brand_name;
+        private $store_id;
+
+        function __construct($brand_name, $store_id, $id = null)
+        {
+            $this->brand_name = $brand_name;
+            $this->store_id = $store_id;
+            $this->id = $id;
+        }
+//getters & setters
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getBrandName()
+        {
+            return $this->brand_name;
+        }
+
+        function getStoreId()
+        {
+            return $this->store_id;
+        }
+
+        //methods
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (brand_name, store_id) VALUES ('{$this->getBrandName()}', '{$this->getStoreId()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        function addStore($store)
+        {
+            // $GLOBALS['DB']->exec("INSERT INTO brands_stores (course_id, student_id) VALUES ({$this->getId()}, {$student->getId()});");
+        }
+
+        function getStudents()
+        {
+            // $returned_students = $GLOBALS['DB']->query("SELECT students.* FROM courses
+            //     JOIN students_courses ON (students_courses.course_id = courses.id)
+            //     JOIN students ON (students.id = students_courses.student_id)
+            //     WHERE courses.id = {$this->getId()};");
+            //
+            // $students = array();
+            //
+            // foreach($returned_students as $student) {
+            //     $name = $student['name'];
+            //     $enrollment_date = $student['enrollment_date'];
+            //     $id = $student['id'];
+            //     $new_student = new Student($name, $enrollment_date, $id);
+            //     array_push($students, $new_student);
+            // }
+            // return $students;
+        }
+
+
+        static function find($search_id)
+        {
+          //   $found_course = null;
+          //   $courses = Course::getAll();
+          //   foreach($courses as $course) {
+          //       $course_id = $course->getId();
+          //       if ($course_id == $search_id) {
+          //           $found_course = $course;
+          //       }
+          //   }
+          //  return $found_course;
+        }
+
+
+//static methods
+        static function getAll()
+        {
+            $returned_courses = $GLOBALS['DB']->query("SELECT * FROM courses;");
+            $courses = array();
+            foreach($returned_courses as $course) {
+                $course_name = $course['course_name'];
+                $course_number = $course['course_number'];
+                $id = $course['id'];
+                $new_course = new Course($course_name, $course_number, $id);
+                array_push($courses, $new_course);
+            }
+            return $courses;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM courses;");
+        }
+    }
+?>
