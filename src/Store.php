a7095bdb58a680ed674718430883342103dab5d0
Katy Henning <katyannhening@gmail.com>
2016-09-30 08:50:12 -0700

    adds initial tests to get and set name of stores

--- src/Store.php
+++ src/Store.php
@@ -1,1 +1,101 @@
-<?php  ?>
+<?php
+    class Store
+    {
+        private $id;
+        private $name;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+//getters & setters
+        function getId()
+        {
+            return $this->id;
+        }
+        function getName()
+        {
+            return $this->name;
+        }
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+//methods
+        function save()
+        {
+            // $GLOBALS['DB']->exec("INSERT INTO students (name, enrollment_date) VALUES ('{$this->getName()}', '{$this->getEnrollmentDate()}');");
+            // $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        function addCourses($course)
+        {
+            // $GLOBALS['DB']->exec("INSERT INTO students_courses (course_id, student_id) VALUES ({$course->getId()}, {$this->getId()});");
+        }
+
+        function getCourses()
+        {
+            // $returned_courses = $GLOBALS['DB']->query("SELECT courses.* FROM students
+            //     JOIN students_courses ON (students_courses.student_id = students.id)
+            //     JOIN courses ON (courses.id = students_courses.course_id)
+            //     WHERE students.id = {$this->getId()};");
+            //
+            // $courses = array();
+            //
+            // foreach($returned_courses as $course) {
+            //     $course_name = $course['course_name'];
+            //     $course_number = $course['course_number'];
+            //     $id = $course['id'];
+            //     $new_course = new Course($course_name, $course_number, $id);
+            //     array_push($courses, $new_course);
+            // }
+            // return $courses;
+        }
+
+        function update($new_name)
+        {
+            // $GLOBALS['DB']->exec("UPDATE students SET name = '{$new_name}' WHERE id = {$this->getId()};");
+            // $this->setName($new_name);
+        }
+
+        function delete()
+        {
+            // $GLOBALS['DB']->exec("DELETE FROM students WHERE id = {$this->getId()};");
+        }
+
+//static methods
+        static function find($search_id)
+        {
+          //   $found_student = null;
+          //   $students = Student::getAll();
+          //   foreach($students as $student) {
+          //       $student_id = $student->getId();
+          //       if ($student_id == $search_id) {
+          //           $found_student = $student;
+          //       }
+          //   }
+          //  return $found_student;
+        }
+
+        static function getAll()
+        {
+            // $returned_students = $GLOBALS['DB']->query("SELECT * FROM students;");
+            // $students = array();
+            // foreach($returned_students as $student) {
+            //     $name = $student['name'];
+            //     $enrollment_date = $student['enrollment_date'];
+            //     $id = $student['id'];
+            //     $new_student = new Student($name, $enrollment_date, $id);
+            //     array_push($students, $new_student);
+            // }
+            // return $students;
+        }
+
+        static function deleteAll()
+        {
+          // $GLOBALS['DB']->exec("DELETE FROM students;");
+        }
+    }
+?>
---
+++ tests/StoreTest.php
@@ -0,0 +1,201 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Store.php";
+
+    $server = 'mysql:host=localhost;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    class StoreTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            // Store::deleteAll();
+        }
+// Test your getters and setters.
+        function test_getStoreName()
+        {
+            //Arrange
+            $id = null;
+            $name = "SW 5th";
+            $test_store = new Store($name, $id);
+            //Act
+            $result = $test_store->getName();
+            //Assert
+            // id is null here, but that is not what we are testing. We are only interested in student name.
+            $this->assertEquals($name, $result);
+        }
+        function test_setName()
+        {
+            //Arrange
+            $id = null;
+            $name = "SW 5th";
+            $test_store = new Store($name, $id);
+
+            $new_name = "SW Washington";
+            //Act
+
+            $test_store->setName($new_name);
+            $result = $test_store->getName();
+
+            //Assert
+            $this->assertEquals($new_name, $result);
+        }
+
+        // function test_getEnrollmentDate()
+        // {
+        //     //Arrange
+        //     // no need to pass in id because it is null by default.
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     //Act
+        //     $result = $test_student->getEnrollmentDate();
+        //     //Assert
+        //     // id is null here, but that is not what we are testing. We are only interested in enrollment date.
+        //     $this->assertEquals($enrollment_date, $result);
+        // }
+        //
+        // function test_setEnrollmentDate()
+        // {
+        //     //Arrange
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     $new_enrollment_date = "2016-09-22";
+        //     //Act
+        //     $test_student->setEnrollmentDate($new_enrollment_date);
+        //     $result = $test_student->getEnrollmentDate();
+        //     //Assert
+        //     $this->assertEquals($new_enrollment_date, $result);
+        // }
+        //
+        // function test_getId()
+        // {
+        //     //Arrange
+        //     $id = 1;
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date, $id);
+        //     //Act
+        //     $result = $test_student->getId();
+        //     //Assert
+        //     $this->assertEquals($id, $result); //make sure id returned is the one we put in, not null.
+        // }
+        //
+        // function test_save()
+        // {
+        //     //Arrange
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     $test_student->save();
+        //     //Act
+        //     $result = Student::getAll();
+        //     //Assert
+        //     $this->assertEquals($test_student, $result[0]);
+        // }
+        //
+        // function test_getAll()
+        // {
+        //     //Arrange
+        //     // create more than one student to make sure getAll returns them all.
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     $test_student->save();
+        //
+        //     $name2 = "helloapro";
+        //     $enrollment_date2 = "2016-04-11";
+        //     $test_student2 = new Student($name2, $enrollment_date2);
+        //     $test_student2->save();
+        //     //Act
+        //     $result = Student::getAll();
+        //     //Assert
+        //     $this->assertEquals([$test_student, $test_student2], $result);
+        // }
+        //
+        // function test_getAllCourses()
+        // {
+        //     //Arrange
+        //     // create more than one course to make sure getAll returns them all.
+        //     $course_name = "History";
+        //     $course_number = "HIS101";
+        //     $test_course = new Course($course_name, $course_number);
+        //     $test_course->save();
+        //
+        //     $course_name1 = "Math";
+        //     $course_number1 = "MT101";
+        //     $test_course1 = new Course($course_name, $course_number);
+        //     $test_course1->save();
+        //
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     $test_student->save();
+        //     $test_student->addCourses($test_course);
+        //
+        //
+        //     //Act
+        //     $result = $test_student->getCourses();
+        //     //Assert
+        //     $this->assertEquals([$test_course], $result);
+        // }
+        //
+        // function testUpdate()
+        // {
+        //     //Arrange
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     $test_student->save();
+        //     $new_name = "helloapro";
+        //     //Act
+        //     $test_student->update($new_name);
+        //     //Assert
+        //     $this->assertEquals("helloapro", $test_student->getName());
+        // }
+        //
+        // function testDelete()
+        // {
+        //     //Arrange
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     $test_student->save();
+        //
+        //     $name2 = "helloapro";
+        //     $enrollment_date2 = "2016-04-11";
+        //     $test_student2 = new Student($name2, $enrollment_date2);
+        //     $test_student2->save();
+        //     //Act
+        //     $test_student->delete();
+        //     $result = Student::getAll();
+        //     //Assert
+        //     $this->assertEquals([$test_student2], $result);
+        // }
+        //
+        // function testFind()
+        // {
+        //     //Arrange
+        //     $name = "KatyCodes";
+        //     $enrollment_date = "2016-09-06";
+        //     $test_student = new Student($name, $enrollment_date);
+        //     $test_student->save();
+        //
+        //     $name2 = "helloapro";
+        //     $enrollment_date2 = "2016-04-11";
+        //     $test_student2 = new Student($name2, $enrollment_date2);
+        //     $test_student2->save();
+        //     //Act
+        //     $result = Student::find($test_student->getId());
+        //     //Assert
+        //     $this->assertEquals($test_student, $result);
+        // }
+
+    }
+?>
